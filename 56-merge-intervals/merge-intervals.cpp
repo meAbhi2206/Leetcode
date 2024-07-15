593ba098d054b5bccf125d2896dd6345c653994d
Abhishek Ranjan <126411191+meAbhi2206@users.noreply.github.com>
2024-07-15 22:09:49 +0530

    Time: 26 ms (41.02%) | Memory: 22.9 MB (29.35%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        for(int i=0;i<n;i++){
+            //current interval does not lie in ans
+            if(ans.empty() || intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }
+            //if current interval lies in ans  
+            else{
+                ans.back()[1]=max(intervals[i][1],ans.back()[1]);
+            }
+        }
+        return ans;
+    }
+};
