03d50dbb8985c231e8099399a1bc3e4f2b23904b
Abhishek Ranjan <126411191+meAbhi2206@users.noreply.github.com>
2024-08-12 22:32:55 +0530

    Time: 61 ms (39.59%) | Memory: 36.3 MB (57.91%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<pair<int, int>> adj[n];
+        for (auto it : roads) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+
+        priority_queue<pair<long long, int>,
+                       vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+
+        vector<long long> dist(n, LLONG_MAX);
+        vector<int> ways(n, 0);
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+
+        int mod = (int)(1e9 + 7);
+        while (!pq.empty()) {
+            long long dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for (auto it : adj[node]) {
+                int adjNode = it.first;
+                int edW = it.second;
+
+                // coming first time
+                if (dis + edW < dist[adjNode]) {
+                    // update dist vector
+                    dist[adjNode] = dis + edW;
+                    // push in pq
+                    pq.push({dis + edW, adjNode});
+                    // ways will be same as previous node
+                    ways[adjNode] = ways[node];
+                }
+                // if we have same dist again via different path
+                // update the ways count only
+                else if (dis + edW == dist[adjNode]) {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+                }
+            }
+        }
+        return ways[n - 1] % mod;
+    }
+};
